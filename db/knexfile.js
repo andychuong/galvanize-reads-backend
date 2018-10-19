c2f054a50d3f767ce7b296ba9abb98707ba427ea
Andy Chuong <andy.chuong@colorado.edu>
2018-10-19 11:04:18 -0600

    dev db connection

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
         client: `pg`,
-        connection: process.env.DATABASE_URL,
+        connection: `postgres://localhost/galvanize_books_dev`,
     }
-} 
+}
