252f782cb6c2ab997025b540a9df985d09b1d9b2
Andy Chuong <andy.chuong@colorado.edu>
2018-10-19 10:40:05 -0600

    authors migration

--- db/migrations/20181019102549_authors.js
+++ db/migrations/20181019102549_authors.js
@@ -1,11 +1,10 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('authors', table => {
-  table.increments()
-  table.string('firstName').notNullable()
-  table.string('lastName').notNullable()
-  table.string('portraitUrl').notNullable().defaultTo('http://placehold.it/600x900')
-})
+    table.increments()
+    table.string('firstName').notNullable()
+    table.string('lastName').notNullable()
+    table.string('portraitUrl').notNullable().defaultTo('http://placehold.it/600x900')
+  })
 };
 
 exports.down = function(knex, Promise) {
